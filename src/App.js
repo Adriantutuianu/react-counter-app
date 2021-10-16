58bf1dd323c0382415f6c2f677ddea7388d52db8
Adrian <adriantutuianu01@gmail.com>
2021-10-17 00:26:44 +0100

    Functionality to decrement

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
+  const handleClickIncrement = () => {
     setCount(count + 1);
+  };
+
+  const handleClickDecrement = () => {
+    setCount(count - 1);
   };
 
   return (
     <div style={{ textAlign: "center" }}>
       <p style={{ color: count % 2 == 0 ? "red" : "green" }}>{count}</p>
-      <button onClick={() => handleClick()}>+1</button>
+      <button onClick={() => handleClickIncrement()}>+1</button>
+      <button onClick={() => handleClickDecrement()}>-1</button>
     </div>
   );
 }
