46fe3548a3b6231e20383c84709bdc85d0f0aaa9
Adrian <adriantutuianu01@gmail.com>
2021-10-17 00:27:44 +0100

    Quick fix

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <p style={{ color: count % 2 == 0 ? "red" : "green" }}>{count}</p>
+      <p style={{ color: count % 2 === 0 ? "red" : "green" }}>{count}</p>
       <button onClick={() => handleClickIncrement()}>+1</button>
       <button onClick={() => handleClickDecrement()}>-1</button>
     </div>
